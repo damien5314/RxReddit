3eecaf4c70968f996171b98ceedb89b402a202c3
damien5314 <damien5314@gmail.com>
2016-03-24 14:58:29 -0700

    Sample app is working

--- app/src/main/java/ddiehl/rxreddit/MainActivity.java
+++ app/src/main/java/ddiehl/rxreddit/MainActivity.java
@@ -1,47 +1,92 @@
 package ddiehl.rxreddit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import ddiehl.rxreddit.sample.BuildConfig;
 import ddiehl.rxreddit.sample.R;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 import rxreddit.api.RedditService;
 import rxreddit.model.Link;
 
 public class MainActivity extends AppCompatActivity {
 
-  private RedditService mRedditService =
-      new RedditService(
-          BuildConfig.REDDIT_APP_ID,
-          BuildConfig.REDDIT_REDIRECT_URI,
-          "DEVICE_ID",
-          String.format("android:ddiehl.rxreddit.sampleapp:%s", BuildConfig.VERSION_NAME),
-          new AndroidAccessTokenManager(this)
-      );
-
   private List<Link> mData = new ArrayList<>();
   private LinkAdapter mLinkAdapter = new LinkAdapter(mData);
+
+  private RedditService mRedditService;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     RecyclerView listView = (RecyclerView) findViewById(R.id.recycler_view);
-    listView.setAdapter(mLinkAdapter);
+    if (listView != null) {
+      listView.setLayoutManager(new LinearLayoutManager(this));
+      listView.setAdapter(mLinkAdapter);
+    }
+
+    mRedditService = new RedditService(
+        BuildConfig.REDDIT_APP_ID,
+        BuildConfig.REDDIT_REDIRECT_URI,
+        getDeviceId(),
+        String.format("android:ddiehl.rxreddit.sampleapp:%s", BuildConfig.VERSION_NAME),
+        new AndroidAccessTokenManager(this)
+    );
   }
 
   @Override
   protected void onStart() {
     super.onStart();
 
+    // Get Observable from RedditService
+    mRedditService.loadLinks("androiddev", "top", "week", null, null)
+
+//        .timeout(5, TimeUnit.SECONDS)
+
+        // Subscribe on proper threads
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+
+        // Transform ListingResponse into list of Links
+        .flatMap(response -> Observable.from(response.getData().getChildren()))
+        .map(listing -> (Link) listing)
+        .toList()
+
+        // Subscribe with callbacks
+        .subscribe(onLinksLoaded(), onError());
+  }
+
+  private Action1<List<Link>> onLinksLoaded() {
+    return links -> {
+      Log.d("RxReddit", "SUCCESS");
+      mData.addAll(links);
+      mLinkAdapter.notifyDataSetChanged();
+    };
+  }
+
+  private Action1<Throwable> onError() {
+    return error -> {
+      Log.e("RxReddit", "ERROR", error);
+      Toast.makeText(this, "an error occurred", Toast.LENGTH_SHORT);
+    };
   }
 
   private static final class LinkAdapter extends RecyclerView.Adapter<LinkViewHolder> {
@@ -56,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     public LinkViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
       return new LinkViewHolder(
           LayoutInflater.from(parent.getContext())
-              .inflate(R.layout.link_layout, parent));
+              .inflate(R.layout.link_layout, parent, false));
     }
 
     @Override
@@ -72,15 +117,32 @@ public class MainActivity extends AppCompatActivity {
 
   private static final class LinkViewHolder extends RecyclerView.ViewHolder {
 
-    private TextView mTitleView;
+    private TextView mTitleText;
+    private TextView mAuthorText;
 
     public LinkViewHolder(View itemView) {
       super(itemView);
-      mTitleView = (TextView) itemView.findViewById(R.id.title_view);
+      mTitleText = (TextView) itemView.findViewById(R.id.title_view);
+      mAuthorText = (TextView) itemView.findViewById(R.id.author_view);
     }
 
     public void bind(Link link) {
-      mTitleView.setText(link.getTitle());
+      mTitleText.setText(link.getTitle());
+      mAuthorText.setText(
+          String.format(
+              itemView.getContext().getString(R.string.author_formatter), link.getAuthor()));
     }
+  }
+
+  private String getDeviceId() {
+    final String PREFS_DEVICE_ID = "prefs_device_id";
+    final String PREF_DEVICE_ID = "device_id";
+    SharedPreferences sp = getSharedPreferences(PREFS_DEVICE_ID, Context.MODE_PRIVATE);
+    String deviceId = sp.getString(PREF_DEVICE_ID, null);
+    if (deviceId == null) {
+      deviceId = UUID.randomUUID().toString();
+      sp.edit().putString(PREF_DEVICE_ID, deviceId).apply();
+    }
+    return deviceId;
   }
 }
